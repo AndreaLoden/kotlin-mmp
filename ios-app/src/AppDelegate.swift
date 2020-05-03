3934f35dcf65785f52c8d4afe869c73953696ea8
AndreaLoddoCS <a.loddo@clevershuttle.de>
2020-05-04 00:10:12 +0200

    Popular movies on iOS

--- ios-app/src/AppDelegate.swift
+++ ios-app/src/AppDelegate.swift
@@ -1,30 +1,20 @@
-/*
- * Copyright 2019 IceRock MAG Inc. Use of this source code is governed by the Apache 2.0 license.
- */
-
 import UIKit
-import MultiPlatformLibrary
 
 @UIApplicationMain
-class AppDelegate: NSObject, UIApplicationDelegate {
-    
+class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
-    
-    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]? = nil) -> Bool {
-        
-        // create factory of shared module - it's main DI component of application.
-        // Provide ViewModels of all features.
-        // Input is platform-specific:
-        // * settings - settings platform storage for https://github.com/russhwolf/multiplatform-settings
-        // * antilog - platform logger with println for https://github.com/AAkira/Napier
-        // * baseUrl - server url from platform build configs (allows use schemes for server configs)
-        // * newsUnitsFactory - platform factory of UITableView items for https://github.com/icerockdev/moko-units
-        AppComponent.factory = SharedFactory(
-            settings: AppleSettings(delegate: UserDefaults.standard),
-            antilog: DebugAntilog(defaultTag: "MPP"),
-            baseUrl: "https://newsapi.org/v2/",
-            newsUnitsFactory: NewsListUnitsFactory()
-        )
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         return true
     }
+
+    func applicationWillResignActive(_ application: UIApplication) {}
+
+    func applicationDidEnterBackground(_ application: UIApplication) {}
+
+    func applicationWillEnterForeground(_ application: UIApplication) {}
+
+    func applicationDidBecomeActive(_ application: UIApplication) {}
+
+    func applicationWillTerminate(_ application: UIApplication) {}
 }
